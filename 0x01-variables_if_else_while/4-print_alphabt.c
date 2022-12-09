e38f5e70a9a8afec83fc2656e92dc7301a3d8cf9
joanwangalwa <wangalwamoha@gmail.com>
2022-12-09 02:53:55 -0800

    Missing aphabet

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Missing aplhabets
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char a;
+
+	while (a <= 'z')
+	{
+		if (a != 'e' && a != 'q')
+		putchar(a);
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}
